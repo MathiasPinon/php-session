21af94558aac84b2199e308d10bcbd4705b42025
Mathias Pinon <mathias.pinon@etudiant.univ-reims.fr>
2023-09-14 09:23:49 +0200

    Ajout des classes Country et CountryCollection

---
+++ Entity/CountryCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+use Database\MyPdo;
+
+class CountryCollection
+{
+    public static function findAll(): array|false
+    {
+        $sql = MyPdo::getInstance()->prepare(
+            <<<SQL
+            SELECT id , code , name 
+            FROM country 
+            ORDER BY name 
+SQL
+        );
+
+        $sql->execute();
+
+        return $sql->fetchAll(\PDO::FETCH_CLASS, Country::class);
+    }
+}
